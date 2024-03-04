88f531fa6b6ebb26ea43e7fdcce70815aa10d57e
Bayu Nugraha <115871496+kabukibayu@users.noreply.github.com>
2024-03-04 12:39:51 +0700

    commit

---
+++ Endpoints/Sample/HelloWorldController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DotnetApiTemplate.WebApi.Endpoints.Sample
+{
+    [Route("api/[controller]")]
+public class HelloWorldController : ControllerBase
+{
+    [HttpGet]
+    [Route("[action]")]
+    public IActionResult SayHello()
+    {
+        return Ok("Hello, TeamCity from ASP.NET Core API!");
+    }
+}
+
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using DotnetApiTemplate.WebApi.Endpoints.Sample;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseHttpsRedirection();
+
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    _ = endpoints.MapControllers(); 
+});
+
+app.Run();
